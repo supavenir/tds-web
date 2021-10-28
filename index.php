77fb7d70a4fe81fdf824a2548db145225be026da
J-C HERON <myaddressmail@gmail.com>
2021-10-28 09:19:54 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Tds PHP</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+	<h1>Essais de la méthode GET</h1>
+	<ul>
+		<li>
+			<a href="test.php?msg=Hello World!">Hello...</a>
+		</li>
+		<li>
+			<a href="test.php?msg=Récupération avec $_GET">GET en php</a>
+		</li>
+	</ul>
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,4 @@
+<h1>Message affiché</h1>
+<?php
+	echo $_GET['msg']??'';
+	
